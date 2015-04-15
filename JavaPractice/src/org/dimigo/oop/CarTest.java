84472466f6421754a3665d1e0aab84c0ebcbb62f
경연 <@KY>
2015-04-15 15:34:00 +0900

    일곱번째 과제 완전판

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -18,16 +18,21 @@ package org.dimigo.oop;
 
 public class CarTest {
 	public static void main(String[] args) {
-		Car2 car1 = new Car2("현대자동차", "제네시스", "검정색", 225, 50000000);
-		Car2 car2 = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
-		Car2 car3 = new Car2("삼성자동차", "SM7", "회색", 200, 38000000);
+//		Car2 car1 = new Car2("현대자동차", "제네시스", "검정색", 225, 50000000);
+//		Car2 car2 = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
+//		Car2 car3 = new Car2("삼성자동차", "SM7", "회색", 200, 38000000);
+		
+		Car3 car1 = new Car3("현대자동차", "제네시스", "검정색", 225, 50000000);
+		Car3 car2 = new Car3("기아자동차", "K7", "흰색", 246);
+		Car3 car3 = new Car3("삼성자동차", "SM7", "회색");
+		
 		
 		//Input(car1, car2, car3);
 		Output(car1, car2, car3);
 
 	}
 	
-	static void Input(Car2 car1, Car2 car2, Car2 car3) {
+	static void Input(Car3 car1, Car3 car2, Car3 car3) {
 		car1.setCompany("현대자동차");
 		car1.setModel("제네시스");
 		car1.setColor("검정색");
@@ -47,7 +52,7 @@ public class CarTest {
 		car3.setPrice(38000000);
 		}
 	
-	static void Output(Car2 car1, Car2 car2, Car2 car3) {
+	static void Output(Car3 car1, Car3 car2, Car3 car3) {
 		System.out.println("<< 자동차 목록 >>");
 		
 		System.out.println("제조사명 : " + car1.getCompany());
