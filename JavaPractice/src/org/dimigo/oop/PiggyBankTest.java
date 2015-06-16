d1bc5978d7ef437d7dbc9f04ac51b5043ad143d0
primeheader <primeheader@KY>
2015-06-16 11:53:29 +0900

    마지막 과제

---
+++ JavaPractice/src/org/dimigo/oop/PiggyBankTest.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *  |_ PiggyBankTest
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 6. 16.
+ * </pre>
+ * 
+ * @author	: primeheader
+ * @version	: 1.0
+ */
+
+public class PiggyBankTest {
+	public static void main(String[] args) {
+		FamilyMember mem1 = new FamilyMember("아빠");
+		FamilyMember mem2 = new FamilyMember("엄마");
+		FamilyMember mem3 = new FamilyMember("나");
+		FamilyMember mem4 = new FamilyMember("남동생");
+		
+		FamilyMember.printMemberCnt();
+		
+		PiggyBank.putMoney(mem1, 10000);
+		PiggyBank.putMoney(mem2, 5000);
+		PiggyBank.putMoney(mem3, 2000);
+		PiggyBank.putMoney(mem4, 1000);
+		
+		PiggyBank.printBalance();
+		
+		PiggyBank.putMoney(mem3, 1000);
+		
+		PiggyBank.printBalance();
+	}
+}
